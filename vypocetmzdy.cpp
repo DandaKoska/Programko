1443f29825859496d3f156ee391e2be75bc0104e
DandaKoska <81356233+DandaKoska@users.noreply.github.com>
2021-03-27 09:33:45 +0100

    Update vypocetmzdy.cpp

--- vypocetmzdy.cpp
+++ vypocetmzdy.cpp
@@ -4,17 +4,19 @@
 int main(){
 int cislo1;
 int cislo2;
+int cislo3;
 
-
-printf("Program na výpočítání platu na brigádě za jeden den \n");
+printf("Program na výpoèítání platu na brigádì za jeden den \n");
 printf("Zadej mzdu za 1h: ");
  scanf("%d",&cislo1);
   printf("zadej odpracovaných hodin:");
     scanf("%d",&cislo2);
- 	int suma = cislo1 * cislo2;
+    printf("Zadej poèet odpracovaných dní :");
+    scanf("%d",&cislo3);
+ 	int suma = cislo1 * cislo2 * cislo3;
     printf( "Tva mzda za den je: %d  ", suma);
-    if( suma > 1000){
-    	printf(" Jsi na to dobre ;D ");
+    if(suma>30000){
+    	printf("Máš jsi blázen :D \n");
 	}
 
 printf("\nII 	I   I");
